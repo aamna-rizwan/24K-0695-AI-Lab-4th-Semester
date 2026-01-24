2df370192443211d67773b344334d735ca63037c
Aamna Rizwan <aamnarizwan7@gmail.com>
2026-01-24 13:12:32 +0500

    Add student grading system based on marks

--- Lab 1 /q1.py
+++ Lab 1 /q1.py
@@ -1,1 +1,12 @@
-
+name =input("Enter your name : ")
+marks=int(input("Enter marks : "))
+print("Student name : " , name)
+print("Marks: ", marks)
+if marks >= 85 and marks <=100:
+    print("Grade : A")
+elif marks >=70 and marks <=84:
+    print("Grade : B ")
+elif marks >=50 and marks <=69:
+    print("Grade : C")
+elif marks <=49 and marks >=0:
+    print("you failed :(")
