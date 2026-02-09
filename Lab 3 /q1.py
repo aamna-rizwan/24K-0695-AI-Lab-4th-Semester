3f8861a0f03c3e5d3dcd861222e02863ea47d205
Aamna Rizwan <aamnarizwan7@gmail.com>
2026-02-10 00:29:32 +0500

    Implement Simple Reflex Agent with Environment

--- Lab 3 /q1.py
+++ Lab 3 /q1.py
@@ -1,1 +1,25 @@
+class Environment: 
+    def __init__ (self,traffic='Heavy'):
+        self.traffic=traffic
+    def get_percept(self):
+        return 'Heavy' if self.traffic=='Heavy' else 'Light'
 
+class SimpleReflexAgent:
+    def __init__ (self):
+        pass
+    def act(self,percept):
+        if percept=="Heavy":
+            return "Green for longer"
+        else:
+            return "Normal green"
+            
+
+def run_agent(agent,environment):
+    percept=environment.get_percept()
+    action=agent.act(percept)
+    print(f"Percept : {percept}, Action : {action}")
+
+agent=SimpleReflexAgent()
+environment=Environment("Heavy")
+
+run_agent(agent,environment)
