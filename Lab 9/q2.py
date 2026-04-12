a6ec1e6042854f54ba8ac2bbc442d31d2eb24778
Aamna Rizwan <aamnarizwan7@gmail.com>
2026-04-12 18:33:12 +0500

    Create q2.py

---
+++ Lab 9/q2.py
@@ -0,0 +1,53 @@
+#task2
+from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+
+
+model = DiscreteBayesianNetwork([
+    ('Intelligence', 'Grade'),
+    ('StudyHours', 'Grade'),
+    ('Difficulty', 'Grade'),
+    ('Grade', 'Pass')
+])
+
+
+cpd_i = TabularCPD(variable='Intelligence', variable_card=2,
+                   values=[[0.7], [0.3]])
+
+cpd_s = TabularCPD(variable='StudyHours', variable_card=2,
+                   values=[[0.6], [0.4]])
+
+cpd_d = TabularCPD(variable='Difficulty', variable_card=2,
+                   values=[[0.4], [0.6]])
+
+cpd_g = TabularCPD(
+    variable='Grade', variable_card=3,
+    values=[
+        [0.7,0.5,0.2,0.1, 0.6,0.4,0.2,0.1],
+        [0.2,0.3,0.4,0.3, 0.3,0.4,0.3,0.3],
+        [0.1,0.2,0.4,0.6, 0.1,0.2,0.5,0.6]
+    ],
+    evidence=['Intelligence','StudyHours','Difficulty'],
+    evidence_card=[2,2,2]
+)
+
+cpd_p = TabularCPD(
+    variable='Pass', variable_card=2,
+    values=[
+        [0.05,0.2,0.5],
+        [0.95,0.8,0.5]
+    ],
+    evidence=['Grade'],
+    evidence_card=[3]
+)
+
+model.add_cpds(cpd_i, cpd_s, cpd_d, cpd_g, cpd_p)
+assert model.check_model()
+infer = VariableElimination(model)
+q1 = infer.query(variables=['Pass'],
+                 evidence={'StudyHours':0, 'Difficulty':0})
+print(q1)
+q2 = infer.query(variables=['Intelligence'],
+                 evidence={'Pass':1})
+print(q2)
